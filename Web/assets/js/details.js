a99007a6caf2459f01debb45bf0a8812917b1b39
Ahmad <ahmedqss120@gmail.com>
2026-07-03 21:40:31 +0300

    Make the product option brand logo container completely transparent and borderless

--- Web/assets/js/details.js
+++ Web/assets/js/details.js
@@ -688,8 +688,8 @@ function FillProductSubCategories(data) {
                                         </div>
                                         <!-- End of Store Content -->
                                         <div class="store-footer">
-                                            <figure class="seller-brand" style="background-color: #F29811;">
-                                                <img src="/assets/images/site_icon.png" alt="not fill image" width="80" height="80">
+                                            <figure class="seller-brand" style="background: transparent !important; border: none !important; box-shadow: none !important;">
+                                                <img src="/assets/images/site_icon.png" alt="not fill image" width="80" height="80" style="object-fit: contain;">
                                             </figure>
                                             <a href="javascript:void(0)" onclick="manageprdouctcart('${item.Token}','${IsArabic ? item.NameAr : item.NameEn}','${Math.ceil((netPriceUsd * rate_value).toFixed(2)) + ' ' + rate_code}');" class="btn btn-dark btn-link btn-underline btn-icon-right btn-visit"><i class="w-icon-cart"></i>${IsArabic ? ' أضف الى عربة التسوق' : ' Add to Cart'}</a>
                                         </div>
